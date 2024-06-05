6dfcd395b184195a1583ad90241b8942f3298395
antlop <lopezanton94@gmail.com>
2024-06-05 12:09:36 -0400

    Dash ability
    In order to get around the map quicker, I added a dash mechanic to the player with a cooldown indicator

--- Lovecraft/Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Lovecraft/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,13 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float rotationSpeed = 100.0f;
     [SerializeField] private InputAction playerMovement;
     [SerializeField] private InputAction playerClick;
+    [SerializeField] private InputAction playerDash;
     [SerializeField] private Transform PlayerVisuals;
+    [SerializeField] private float dashDistance = 5;
+    [SerializeField] private float dashCooldown = 3;
+    [SerializeField] private Image dashCooldownIndicator;
 
     private NavMeshAgent agent;
     private PathfindingCostGrid pfindCostGrid;
     private Vector2 movementInput;
     private bool isMovingWithInput;
     private bool isRotating = true;
+    private bool isDashing = false; 
+    private float dashCooldownBucket = 0;
 
     private void Start()
     {
@@ -29,8 +36,10 @@ public class PlayerController : MonoBehaviour
         playerMovement.performed += OnMove;
         playerMovement.canceled += OnMoveCanceled;
         playerClick.performed += OnClick;
+        playerDash.performed += PerformDash;
         playerMovement.Enable();
         playerClick.Enable();
+        playerDash.Enable();
     }
 
     private void OnDisable()
@@ -40,11 +49,35 @@ public class PlayerController : MonoBehaviour
         playerClick.performed -= OnClick;
         playerMovement.Disable();
         playerClick.Disable();
+        playerDash.Disable();
     }
 
     private void Update()
     {
-        if (isMovingWithInput)
+        if( dashCooldownBucket < dashCooldown)
+        {
+            dashCooldownBucket += Time.deltaTime;
+            dashCooldownIndicator.fillAmount = dashCooldownBucket / dashCooldown;
+        } else
+        {
+            dashCooldownIndicator.enabled = false;
+        }
+
+        if( isDashing )
+        {
+            if (!agent.pathPending)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        // Done
+                        isDashing = false;
+                        agent.speed = 12;
+                    }
+                }
+            }
+        } else if (isMovingWithInput)
         {
             Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
             moveDirection = transform.TransformDirection(moveDirection);
@@ -62,6 +95,7 @@ public class PlayerController : MonoBehaviour
                 PlayerVisuals.LookAt(LookAtPos);
             }
         }
+
 
         // Debug line to show forward direction
         Debug.DrawRay(transform.position, PlayerVisuals.forward * 2, Color.red);
@@ -84,5 +118,39 @@ public class PlayerController : MonoBehaviour
 
     private void OnClick(InputAction.CallbackContext context)
     {
+    }
+
+    private void PerformDash(InputAction.CallbackContext context)
+    {
+        if( dashCooldownBucket < dashCooldown) { return; }
+
+        RaycastHit hit;
+        Vector3 projectedPosition = transform.position;
+        if (Physics.SphereCast(PlayerVisuals.position, 2, PlayerVisuals.forward, out hit, dashDistance))
+        {
+            Vector3 pos = hit.point;
+            Vector3 adjustment = (PlayerVisuals.position - pos).normalized;
+            pos += adjustment;
+
+            projectedPosition = pos;
+            Debug.Log("hit");
+        } else
+        {
+            projectedPosition = PlayerVisuals.position + PlayerVisuals.forward * dashDistance;
+            //transform.position = projectPos;
+
+        }
+       
+        agent.SetDestination(projectedPosition);
+        agent.speed = 100;       
+        isDashing = true;
+        dashCooldownBucket = 0;
+
+        dashCooldownIndicator.enabled = true;
+    }
+
+    public void External_Dash()
+    {
+        PerformDash(new InputAction.CallbackContext());
     }
 }
